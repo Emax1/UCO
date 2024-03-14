69474c355ee92d218ea4d8455619672ead30303c
EMANUEL MUÑOZ <emanuel.guillo@gmail.com>
2024-03-14 10:50:35 +0100

    Update RS_ESMA.py

--- RS_ESMA.py
+++ RS_ESMA.py
@@ -192,7 +192,8 @@ def DF_VALORADOS(viviendas,deseos,id_deseo,pesos,n_deseos):
     df_valorado = pd.DataFrame()
     for i in range(len(n_deseos)):
         df0=MUESTRA_VIVIENDAS(viviendas,deseos.iloc[[n_deseos[i]]],id_deseo,pesos)
-        df_valorado = df_valorado.append(df0, ignore_index=True)
+        #df_valorado = df_valorado.append(df0, ignore_index=True)
+        df_valorado = pd.concat([df_valorado, df0], ignore_index=True)
     relevantes=Counter(df_valorado['Relevante'])['relevante']
     norelevantes=Counter(df_valorado['Relevante'])['no relevante']
     precision=relevantes/norelevantes*100
